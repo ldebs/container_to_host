2d1c0f9d43c17af336e61c5c72696eaae352a714
ldebs <ldebs@ldebs.org>
2024-12-16 12:11:50 +0100

    Simplify run_host

--- container/scripts/2_run_host/1_run_host.sh
+++ container/scripts/2_run_host/1_run_host.sh
@@ -7,26 +7,17 @@ run_host(){
   while true; do cat $HOST_ERR >&2; done &
   epid=$!
 
-  # Capture status result
-  res=$(mktemp -u)
-  $(cat $HOST_STS > $res) &
-  spid=$!
-
   # Send command to host
   echo "$@" > $HOST_CMD
 
-  # Wait for the background process to finish reading status from HOST_STS
-  wait $spid
+  # Wait for status result
+  status=$(cat $HOST_STS)
 
   # Kill and wait the read processes
   kill $opid $epid
   wait $opid > /dev/null 2>&1
   wait $epid > /dev/null 2>&1
   
-  # capture and delete the result
-  status=$(cat $res)
-  rm $res
-
   log "$HC#### end status $status$N"
   sync
   return $status
